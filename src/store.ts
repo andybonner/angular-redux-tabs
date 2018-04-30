91cbd4ac2244f8ed0150d189072515a03d5e3b57
Andy Bonner <abonner@duke.edu>
2018-04-30 10:11:59 -0400

    remove comments

--- src/store.ts
+++ src/store.ts
@@ -5,9 +5,9 @@ import { tabData, tabData2 } from './sampleData';
 
 export interface IAppState {
   populated: boolean;
-  tabsArray: any; // what type IS most appropriate for an array of objects?
+  tabsArray: object;
   activeTabIndex: number;
-  activeTabContent: string; // or is there another type for HTML?
+  activeTabContent: string;
 }
 
 export const INITIAL_STATE: IAppState = {
@@ -20,11 +20,9 @@ export const INITIAL_STATE: IAppState = {
 // In a more complex app I would probably want to break reducers out into their own file
 export function rootReducer(lastState: IAppState, action: AnyAction): IAppState {
   switch (action.type) {
-    // How can I get a nice array of sample data in here and reference it?
     case TabActions.POPULATE: return {
       populated: true,
       tabsArray: action.payload,
-      // aren't the next 2 lines kinda boilerplatey? Is there a better way of saying "all other props stay the same"?
       activeTabIndex: 0,
       activeTabContent: action.payload[0].content
     };
@@ -34,7 +32,6 @@ export function rootReducer(lastState: IAppState, action: AnyAction): IAppState 
       activeTabIndex: action.payload,
       activeTabContent: lastState.tabsArray[action.payload].content
     };
+    default: return lastState;
   }
-
-  return lastState; // Do I need this?
 }
